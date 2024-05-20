a61b60ddc72a0b9602721e4b1185b34d9364874e
dimsumws <134210733+dimsumws@users.noreply.github.com>
2024-05-20 19:15:33 +0800

    Revert "ASRManager added whisper"
    This reverts commit b0fcb78a901be89f8dee31a3e29c9b86ab735454.

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -1,12 +1,8 @@
-import whisper
-
 class ASRManager:
     def __init__(self):
         # initialize the model here
-        model = whisper.load_model("base")
         pass
 
     def transcribe(self, audio_bytes: bytes) -> str:
         # perform ASR transcription
-        result = self.model.transcribe('data/audio.wav', fp16 = False)
-        return result['text']
+        return ""

dae035817cacbba0133352945124ae93b6b909e5
dimsumws <134210733+dimsumws@users.noreply.github.com>
2024-05-23 00:57:04 +0800

    Model 2

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -7,9 +7,16 @@ class ASRManager:
         pass
 
     def transcribe(self, audio_bytes: bytes) -> str:
+        # generate audio file
+        audiopath = "temp.wav"
+        inputAudio = open(audiopath, "wb")
+        inputAudio.write(audio_bytes)
+
+        # noise reduction
+        audio, rate = librosa.load(audiopath, sr=None)
+        noisy_part = audio[0:int(rate*0.3)]  # Identify the noisy part
+        reduced_noise_audio = nr.reduce_noise(y=audio, sr=rate, y_noise=noisy_part)
+
         # perform ASR transcription
-        audio = open("temp.wav", "wb")
-        audio.write(audio_bytes)
-        result = self.model.transcribe("temp.wav")
+        result = self.model.transcribe(reduced_noise_audio)
         return result['text']
-        #return "test"

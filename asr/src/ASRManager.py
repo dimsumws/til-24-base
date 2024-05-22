716af8434cc0587078209ce8dccac7c41285d85c
dimsumws <134210733+dimsumws@users.noreply.github.com>
2024-05-23 02:19:14 +0800

    Model 3

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -3,20 +3,12 @@ import whisper
 class ASRManager:
     def __init__(self):
         # initialize the model here
-        self.model = whisper.load_model("base")
+        self.model = whisper.load_model("base.en")
         pass
 
     def transcribe(self, audio_bytes: bytes) -> str:
-        # generate audio file
-        audiopath = "temp.wav"
-        inputAudio = open(audiopath, "wb")
-        inputAudio.write(audio_bytes)
-
-        # noise reduction
-        audio, rate = librosa.load(audiopath, sr=None)
-        noisy_part = audio[0:int(rate*0.3)]  # Identify the noisy part
-        reduced_noise_audio = nr.reduce_noise(y=audio, sr=rate, y_noise=noisy_part)
-
         # perform ASR transcription
-        result = self.model.transcribe(reduced_noise_audio)
+        audio = open("temp.wav", "wb")
+        audio.write(audio_bytes)
+        result = self.model.transcribe("temp.wav")
         return result['text']

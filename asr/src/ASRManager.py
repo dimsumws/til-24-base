5c836d27f7620ec54bb81cfe7663d492e7ec1e7b
dimsumws <134210733+dimsumws@users.noreply.github.com>
2024-05-24 18:18:55 +0800

    model8 final?

--- asr/src/ASRManager.py
+++ asr/src/ASRManager.py
@@ -1,14 +1,25 @@
-import whisper
+from transformers import WhisperProcessor, WhisperForConditionalGeneration
+from datasets import load_dataset
+from pathlib import Path
+import librosa
 
 class ASRManager:
     def __init__(self):
         # initialize the model here
-        self.model = whisper.load_model("base.en")
+        model_path = Path(f"asrModel8")
+        proc_path = Path(f"asrModel8proc")
+        self.model = WhisperForConditionalGeneration.from_pretrained(model_path)
+        self.processor = WhisperProcessor.from_pretrained(proc_path)
         pass
 
     def transcribe(self, audio_bytes: bytes) -> str:
-        # perform ASR transcription
-        audio = open("temp.wav", "wb")
-        audio.write(audio_bytes)
-        result = self.model.transcribe("temp.wav")
-        return result['text']
+        # generate audio file
+        audiopath = "temp.wav"
+        inputAudio = open(audiopath, "wb")
+        inputAudio.write(audio_bytes)
+        
+        audio_data, sampling_rate = librosa.load(audiopath, sr=None)
+        input_features = self.processor(audio_data, sampling_rate=sampling_rate, return_tensors="pt").input_features
+        predicted_ids = self.model.generate(input_features)
+        transcription = self.processor.batch_decode(predicted_ids, skip_special_tokens=True)
+        return transcription[0]
